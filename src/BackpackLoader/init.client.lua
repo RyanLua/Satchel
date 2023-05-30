90fc8c9647fb360ecef537626499f5bf69aeae1b
Ryan Lua <80087248+RyanLua@users.noreply.github.com>
2023-05-30 14:03:02 -0700

    Update loader
    Signed-off-by: Ryan Lua <80087248+RyanLua@users.noreply.github.com>

--- src/BackpackLoader/init.client.lua
+++ src/BackpackLoader/init.client.lua
@@ -1,3 +1,28 @@
--- @thebrickplanetboy, 2022
+--!strict
 
-require(script:WaitForChild("BackpackScript"))
+--[[
+	Name: Satchel Loader
+	Description: Loads the Satchel backpack system.
+
+	GitHub: https://github.com/RyanLua/Satchel
+]]
+
+--[[
+	Satchel, a modern open-source alternative to Roblox's default backpack.
+	Copyright (C) 2023  RyanLua
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]
+
+require(script:WaitForChild("Satchel"))
